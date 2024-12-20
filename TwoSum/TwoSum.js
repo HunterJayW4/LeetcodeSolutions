8856eb4f2bc6b18835bade622f5d2c9a82a042d3
HunterJayW4 <112739875+HunterJayW4@users.noreply.github.com>
2024-12-20 14:02:33 -0700

    Update TwoSum.js

--- TwoSum/TwoSum.js
+++ TwoSum/TwoSum.js
@@ -5,7 +5,10 @@
  */
 var twoSum = function(nums, target) {
     for (var i = 0; i < nums.length - 1; i++) {
-        if (nums[i] + nums[i + 1] == target)
-            return [i,i + 1];
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]; // Return an array of integers
+            }
+        }
     }
 };
